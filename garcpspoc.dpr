325478dc0e93a6cebaed5aebf3c72fa4a32bc335
André Schnabel <andreschnabel@me.com>
2015-07-29 12:14:27 +0200

    Updated RAD Studio project file

--- garcpspoc.dpr
+++ garcpspoc.dpr
@@ -9,7 +9,6 @@ uses
   main in 'src\main.pas',
   globals in 'src\globals.pas',
   helpers in 'src\helpers.pas',
-  peakcrossover in 'src\Methods\peakcrossover.pas',
   profit in 'src\profit.pas',
   projectdata in 'src\projectdata.pas',
   bbrsm in 'src\Sampling\bbrsm.pas',
@@ -19,7 +18,6 @@ uses
   ssgs in 'src\SGS\ssgs.pas',
   ssgsmod in 'src\SGS\ssgsmod.pas',
   ssgsoc in 'src\SGS\ssgsoc.pas',
-  sgsminoc in 'src\SGS\sgsminoc.pas',
   gassgsoc in 'src\GeneticAlgorithms\gassgsoc.pas',
   gassgsbeta in 'src\GeneticAlgorithms\gassgsbeta.pas',
   topsort in 'src\topsort.pas',
@@ -29,10 +27,9 @@ uses
   gassgsz in 'src\GeneticAlgorithms\gassgsz.pas',
   gassgstau in 'src\GeneticAlgorithms\gassgstau.pas',
   gassgszt in 'src\GeneticAlgorithms\gassgszt.pas',
-  fbi in 'src\Methods\fbi.pas',
   tests in 'src\tests.pas',
   compfitness in 'src\GeneticAlgorithms\compfitness.pas',
-  branchandbound in 'src\Exact\branchandbound.pas';
+  stopwatch in 'src\stopwatch.pas';
 
 var main: TMain;
 begin
